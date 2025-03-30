957ce15815218531e48a3b385d79d97a19304c14
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-03-30 15:34:35 +0530

    Added dimensions for frame

--- detection.py
+++ detection.py
@@ -16,6 +16,8 @@ while True:
     if not ret:
         break
 
+    h, w = frame.shape[:2]
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = face_casscade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
