0afcecd3588197f80d5b338e789faa3350097f32
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-03-30 15:01:00 +0530

    Fixed the lines

--- detection.py
+++ detection.py
@@ -16,4 +16,4 @@ while True:
     faces = face_casscade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
     for(x, y, w, h) in faces:
-        cv2.rectngle(frame, (x, y), (x+h, y+w), (255, 0, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
